2ab65fa9c3642baa0aa2651326890d588c15ede1
aayushgundel <aayushgundel17@gmail.com>
2024-07-25 13:10:28 +0530

    create a branch

---
+++ branch1.py
@@ -0,0 +1,12 @@
+# Function to add two numbers
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# Input numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Call the function and store the result
+result = add_numbers(num1, num2)
+
+# Print the result
